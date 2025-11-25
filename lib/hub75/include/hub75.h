aca738f1586365400c85fd250843f635ca756c8b
lmilz <milzlars88@gmail.com>
2025-11-25 22:05:12 +0100

    include

--- lib/hub75/include/hub75.h
+++ lib/hub75/include/hub75.h
@@ -34,8 +34,30 @@
  */
 
 // GPIO pins
-#define R1_PIN 25 // Red channel for the upper panel row
-#define G1_PIN 26 // Green channel for the upper panel row
-#define B1_PIN 27 // Blue channel for the upper panel row:wq
+#define R1_PIN  25 // Red channel for the upper panel row
+#define G1_PIN  26 // Green channel for the upper panel row
+#define B1_PIN  27 // Blue channel for the upper panel row
+#define R2_PIN  14 // Red channel for the lower panel
+#define G1_PIN  12 // Green channel for the lower panel
+#define B1_PIN  13 // Blue channel for the lower panel
+#define LA_PIN  17 // Address line A (bit 0)
+#define LB_PIN  16 // Address line B (bit 1)
+#define LC_PIN  4  // Address line C (bit 2)
+#define LD_PIN  2  // Address line D (bit 3)
+#define LE_PIN  15 // Address line E (bit 4, for extended adrdressing)
+#define CLK_PIN 18 // Clock signal
+#define LAT_PIN 19 // Latch signal
+#define OE_PIN  21 // Output Enable signal
+		   
+// Panel dimensions (e.g. 64x64)
+#define PANEL_WIDTH  64
+#define PANEL_HEIGHT 64
+
+// Framebuffer
+uint16_t hub75_framebuffer[PANEL_WIDTH * PANEL_HEIHT];
+
+void hub75_init();
+void hub75_set_pixel(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b);
+void hub75_clear_framebuffer();
 
 #endif // HUB75_H
