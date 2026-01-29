8510619f8bca876a942d64ba23720a8bd313cf4d
lmilz <milzlars88@gmail.com>
2026-01-29 22:04:00 +0100

    delete build

--- test/build/CMakeFiles/hub75_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hub75_tests.
-# This may be replaced when dependencies are built.
--- test/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lmilz/Second-Brain/Projects/esp-hub75/test
-# Build directory: /home/lmilz/Second-Brain/Projects/esp-hub75/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/lmilz/Second-Brain/Projects/esp-hub75/test/build/hub75_tests[1]_include.cmake")
